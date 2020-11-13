b65a1f2123dd8cfcdce4d331d680aab7e05a9a6b
Olga Nevezina <nevezhinao@yandex.ru>
2020-11-13 19:20:47 +0300

    Updated Roport.md

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 480;
+        int transfer_amount = 2_148_000_000;
+        int total = balance + transfer_amount;
+        System.out.println("total:");
+        System.out.println(total);
+    }
+}
